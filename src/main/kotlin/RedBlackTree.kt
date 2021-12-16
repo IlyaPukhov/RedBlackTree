b1473e270fffb4712337015a843bb6e4b5629f60
Ilya Pukhov <IlyaPukhov@github.com>
2021-12-16 16:48:05 +0300

    Fixed bugs

--- src/main/kotlin/RedBlackTree.kt
+++ src/main/kotlin/RedBlackTree.kt
@@ -9,9 +9,6 @@ class RedBlackTree<T : Comparable<T>> {
     private fun setRed(node: RedBlackNode<T>?) {
         if (node != null) node.color = RedBlackColor.RED
     }
-    private fun isBlack(node: RedBlackNode<T>?): Boolean {
-        return node?.color == RedBlackColor.BLACK
-    }
     private fun setBlack(node: RedBlackNode<T>?) {
         if (node != null) node.color = RedBlackColor.BLACK
     }
@@ -50,31 +47,30 @@ class RedBlackTree<T : Comparable<T>> {
 
             // Если родительский узел является левым дочерним узлом дедушкиного узла
             if (parent == gparent?.left) {
-                val uncle: RedBlackNode<T>? = gparent?.right // Получить узел дяди
+                val uncle: RedBlackNode<T>? = gparent?.right // Получаем узел дяди
                 // case1: узел дяди тоже красный
                 if (uncle != null && isRed(uncle)) {
-                    setBlack(parent) // Очернить родительский узел и узел дяди
+                    setBlack(parent) // Делаем черными родительский узел и узел дяди
                     setBlack(uncle)
                     setRed(gparent) // Закрашиваем дедушку в красный цвет
                     node = gparent // Помещаем позицию на дедушку узла
                     continue  // Продолжить while
                 }
-
                 // case2: узел-дядя черный, а текущий узел - левый дочерний узел
-                if (isBlack(uncle) && node == parent?.left) {
+                if (node == parent?.left) {
                     setBlack(parent)
                     setRed(gparent)
                     rightRotate(gparent)
                 }
                 // case3: узел-дядя черный, а текущий узел - правый дочерний узел
-                if (isBlack(uncle) && node == parent?.right) {
+                if (node == parent?.right) {
                     leftRotate(parent) // Повернуть влево от родительского узла
                     val tmp: RedBlackNode<T> = parent // Затем меняем местами родительский узел и себя, чтобы подготовиться к следующему правому вращению
                     parent = node
                     node = tmp
                 }
             } else {
-                val uncle: RedBlackNode<T>? = gparent?.left // Получить узел дяди
+                val uncle: RedBlackNode<T>? = gparent?.left // Получаем узел дяди
                 // case1: узел дяди тоже красный
                 if (uncle != null && isRed(uncle)) {
                     setBlack(parent)
@@ -83,15 +79,14 @@ class RedBlackTree<T : Comparable<T>> {
                     node = gparent
                     continue
                 }
-
                 // case4: узел-дядя черный, а текущий узел - правый дочерний узел
-                if (isBlack(uncle) && node == parent?.right) {
+                if (node == parent?.right) {
                     setBlack(parent)
                     setRed(gparent)
                     leftRotate(gparent)
                 }
                 // case5: узел-дядя черный, а текущий узел - левый дочерний узел
-                if (isBlack(uncle) && node == parent?.left) {
+                if (node == parent?.left) {
                     rightRotate(parent)
                     val tmp: RedBlackNode<T> = parent
                     parent = node
@@ -99,7 +94,6 @@ class RedBlackTree<T : Comparable<T>> {
                 }
             }
         }
-
         // Устанавливаем корневой узел в черный цвет
         setBlack(this.root)
     }
@@ -113,22 +107,22 @@ class RedBlackTree<T : Comparable<T>> {
     *   ly ry               lx ly
     **/
     private fun leftRotate(x: RedBlackNode<T>?) {
-        // 1. Назначьте левый дочерний узел y правому дочернему узлу x и назначьте x родительскому узлу левого дочернего узла y (когда левый дочерний узел y не пуст)
+        // 1. Назначаем левый дочерний узел y правому дочернему узлу x и назначаем x родительскому узлу левого дочернего узла y (когда левый дочерний узел y не пуст)
         val y: RedBlackNode<T> = x?.right!!
         x.right = y.left
         if (y.left != null) y.left?.parent = x
 
-        // 2. Назначьте родительский узел p элемента x (если он не пустой) родительскому узлу y и обновите дочерний узел p до y (слева или справа)
+        // 2. Назначаем родительский узел p элемента x (если он не пустой) родительскому узлу y и обновляем дочерний узел p до y (слева или справа)
         y.parent = x.parent
         if (x.parent == null) {
-            this.root = y // Если родительский узел x пуст, установите y как родительский узел
+            this.root = y // Если родительский узел x пуст, устанавливаем y как родительский узел
         } else {
             if (x == x.parent?.left) // Если x левый дочерний узел
                 x.parent?.left = y // Затем также устанавливаем y как левый дочерний узел
-            else x.parent?.right = y // в противном случае установите y как правый дочерний узел
+            else x.parent?.right = y // в противном случае устанавливаем y как правый дочерний узел
         }
 
-        // 3. Установите левый дочерний узел y на x, а родительский узел x на y
+        // 3. Устанавливаем левый дочерний узел y на x, а родительский узел x на y
         y.left = x
         x.parent = y
     }
@@ -142,22 +136,22 @@ class RedBlackTree<T : Comparable<T>> {
     * lx  rx                   rx ry
     **/
     private fun rightRotate(y: RedBlackNode<T>?) {
-        // 1. Назначьте левый дочерний узел y правому дочернему узлу x и назначьте x родительскому узлу левого дочернего узла y (когда левый дочерний узел y не пуст)
+        // 1. Назначаем левый дочерний узел y правому дочернему узлу x и назначаем x родительскому узлу левого дочернего узла y (когда левый дочерний узел y не пуст)
         val x: RedBlackNode<T> = y?.left!!
         y.left = x.right
         if (x.right != null) x.right?.parent = y
 
-        // 2. Назначьте родительский узел p элемента x (если он не пуст) родительскому узлу y и обновите дочерний узел p до y (слева или справа)
+        // 2. Назначаем родительский узел p элемента x (если он не пуст) родительскому узлу y и обновляем дочерний узел p до y (слева или справа)
         x.parent = y.parent
         if (y.parent == null) {
-            this.root = x // Если родительский узел x пуст, установите y как родительский узел
+            this.root = x // Если родительский узел x пуст, устанавливаем y как родительский узел
         } else {
             if (y === y.parent?.right) // Если x левый дочерний узел
-                y.parent?.right = x // Затем также установите y как левый дочерний узел
-            else y.parent?.left = x // в противном случае установите y как правый дочерний узел
+                y.parent?.right = x // Затем также устанавливаем y как левый дочерний узел
+            else y.parent?.left = x // в противном случае устанавливаем y как правый дочерний узел
         }
 
-        // 3. Установите левый дочерний узел y на x, а родительский узел x на y
+        // 3. Устанавливаем левый дочерний узел y на x, а родительский узел x на y
         x.right = y
         y.parent = x
     }
